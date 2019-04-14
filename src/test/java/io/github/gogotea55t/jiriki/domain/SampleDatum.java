7b18d039c611f54e8872ae2795aa05f403c08202
gogotea55t <chirigami05@gmail.com>
2019-04-14 21:49:08 +0900

    テストの初期設定

---
+++ src/test/java/io/github/gogotea55t/jiriki/domain/SampleDatum.java
@@ -0,0 +1,76 @@
+package io.github.gogotea55t.jiriki.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.gogotea55t.jiriki.domain.entity.Scores;
+import io.github.gogotea55t.jiriki.domain.entity.Songs;
+import io.github.gogotea55t.jiriki.domain.entity.Users;
+import lombok.Getter;
+
+@Getter
+public class SampleDatum {
+  private List<Users> users;
+  private List<Songs> songs;
+  private List<Scores> scores;
+
+  public SampleDatum() {
+    Users user1 = new Users();
+    user1.setUserId("u001");
+    user1.setUserName("妖怪1");
+
+    Users user2 = new Users();
+    user2.setUserId("u002");
+    user2.setUserName("妖怪2");
+
+    Songs mitemite = new Songs();
+    mitemite.setSongId("001");
+    mitemite.setJirikiRank("地力Ｓ＋");
+    mitemite.setInstrument("チェンバロ");
+    mitemite.setSongName("みてみて☆こっちっち");
+    mitemite.setContributor("エメラル");
+
+    Songs miraclePaint = new Songs();
+    miraclePaint.setSongId("002");
+    miraclePaint.setJirikiRank("地力Ａ＋");
+    miraclePaint.setSongName("ミラクルペイント");
+    miraclePaint.setContributor("タタナミ");
+    miraclePaint.setInstrument("ピアノ①");
+
+    Scores score1 = new Scores();
+    score1.setUsers(user1);
+    score1.setSongs(mitemite);
+    score1.setScore(99);
+    score1.setScoreId(1);
+
+    Scores score2 = new Scores();
+    score2.setUsers(user2);
+    score2.setSongs(mitemite);
+    score2.setScore(94);
+    score2.setScoreId(2);
+
+    Scores score3 = new Scores();
+    score3.setUsers(user1);
+    score3.setSongs(miraclePaint);
+    score3.setScore(99);
+    score3.setScoreId(2);
+
+    List<Users> users = new ArrayList<Users>();
+    users.add(user1);
+    users.add(user2);
+
+    List<Songs> songs = new ArrayList<Songs>();
+    songs.add(mitemite);
+    songs.add(miraclePaint);
+
+    List<Scores> scores = new ArrayList<Scores>();
+    scores.add(score1);
+    scores.add(score2);
+    scores.add(score3);
+
+    this.users = users;
+    this.songs = songs;
+    this.scores = scores;
+  }
+
+}
