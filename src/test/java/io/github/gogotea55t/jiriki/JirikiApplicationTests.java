32d54c17e8ea5405344c1d8535924b72168b9f61
gogotea55t <chirigami05@gmail.com>
2019-04-19 22:05:22 +0900

    テストアノテーションを正確に

--- src/test/java/io/github/gogotea55t/jiriki/JirikiApplicationTests.java
+++ src/test/java/io/github/gogotea55t/jiriki/JirikiApplicationTests.java
@@ -3,9 +3,11 @@ package io.github.gogotea55t.jiriki;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("unittest")
 @SpringBootTest
 public class JirikiApplicationTests {
 
