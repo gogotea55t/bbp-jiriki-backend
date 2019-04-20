4b87fde39ce1a8390b442ab64be0be78f5383490
gogotea55t <chirigami05@gmail.com>
2019-04-21 08:16:05 +0900

    fixed #10

--- src/main/java/io/github/gogotea55t/jiriki/domain/Score4UserResponse.java
+++ src/main/java/io/github/gogotea55t/jiriki/domain/Score4UserResponse.java
@@ -2,6 +2,7 @@ package io.github.gogotea55t.jiriki.domain;
 
 import io.github.gogotea55t.jiriki.domain.entity.Scores;
 import io.github.gogotea55t.jiriki.domain.entity.Songs;
+import io.github.gogotea55t.jiriki.domain.vo.JirikiRank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,8 @@ import lombok.NoArgsConstructor;
 @Data
 public class Score4UserResponse {
   private String songId;
+  
+  private String jirikiRank;
 
   private String songName;
 
@@ -23,6 +26,7 @@ public class Score4UserResponse {
     Songs song = score.getSongs();
 
     response.setSongId(song.getSongId());
+    response.setJirikiRank(song.getJirikiRank().getJirikiRank());
     response.setContributor(song.getContributor());
     response.setSongName(song.getSongName());
     response.setInstrument(song.getInstrument());
@@ -33,9 +37,10 @@ public class Score4UserResponse {
 
   public Score4UserResponse(Object[] obj) {
 	this.songId = (String)obj[0];
-	this.songName = (String)obj[1];
-	this.contributor = (String)obj[2];
-	this.instrument = (String)obj[3];
-	this.score = (Integer)obj[4];
+	this.jirikiRank = JirikiRank.getJirikiRankFromId((int)obj[1]).getJirikiRank();
+	this.songName = (String)obj[2];
+	this.contributor = (String)obj[3];
+	this.instrument = (String)obj[4];
+	this.score = (Integer)obj[5];
   }
 }
