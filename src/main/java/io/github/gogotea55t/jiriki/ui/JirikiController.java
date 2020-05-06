ab625f6d87e3f28fc23fa81353e85c19ef7f94cb
gogotea55t <chirigami05@gmail.com>
2020-05-06 18:57:45 +0900

    feat: スコア情報を返すときに最高点と平均点も返す機能を追加

--- src/main/java/io/github/gogotea55t/jiriki/ui/JirikiController.java
+++ src/main/java/io/github/gogotea55t/jiriki/ui/JirikiController.java
@@ -23,6 +23,7 @@ import io.github.gogotea55t.jiriki.domain.JirikiService;
 import io.github.gogotea55t.jiriki.domain.Score4SongResponse;
 import io.github.gogotea55t.jiriki.domain.Score4SongResponseV2;
 import io.github.gogotea55t.jiriki.domain.Score4UserResponse;
+import io.github.gogotea55t.jiriki.domain.Score4UserResponseV2;
 import io.github.gogotea55t.jiriki.domain.SongsResponse;
 import io.github.gogotea55t.jiriki.domain.UserResponse;
 import io.github.gogotea55t.jiriki.domain.request.PageRequest;
@@ -165,6 +166,35 @@ public class JirikiController {
     } else {
     }
     List<Score4UserResponse> response = jirikiService.searchScoresByQuery(id, query, pageReq);
+    if (response == null) {
+      return ResponseEntity.notFound().build();
+    } else {
+      return ResponseEntity.ok(response);
+    }
+  }
+  
+  @GetMapping("/v2/players/{id}/scores")
+  public ResponseEntity<?> getScoresByPlayerIdV2(
+      @PathVariable(name = "id") String id,
+      @RequestParam(required = false) String name,
+      @RequestParam(required = false) String contributor,
+      @RequestParam(required = false) String instrument,
+      @RequestParam(required = false) String jiriki,
+      @RequestParam(required = false, defaultValue = "0") Integer page,
+      @RequestParam(required = false, defaultValue = "20") Integer limit) {
+    PageRequest pageReq = new PageRequest(page, limit);
+    Map<String, String> query = new HashMap<String, String>();
+    if (name != null) {
+      query.put("name", name);
+    } else if (contributor != null) {
+      query.put("contributor", contributor);
+    } else if (instrument != null) {
+      query.put("instrument", instrument);
+    } else if (jiriki != null) {
+      query.put("jiriki", jiriki);
+    } else {
+    }
+    List<Score4UserResponseV2> response = jirikiService.searchScoresByQueryV2(id, query, pageReq);
     if (response == null) {
       return ResponseEntity.notFound().build();
     } else {
